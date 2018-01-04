a67565b53f38798c3da8e1fc7b739d672bca170b
Jiang Huang <jiang.huang@hotmail.com>
2018-01-03 23:43:08 -0500

    Add printDecimal

--- src/com/algorithms/backtracking/printBinary.java
+++ src/com/algorithms/backtracking/printBinary.java
@@ -11,8 +11,18 @@ public class printBinary {
 
     }
 
+    public void printDecimal(int digits, String prefix) {
+        if(digits == 0) {
+            System.out.println(prefix);
+        }else{
+            for(int i = 0; i <= 9; i++)
+                printDecimal(digits - 1, prefix + i);
+        }
+    }
+
     public static void main(String[] args) {
         printBinary pb = new printBinary();
         pb.printBinary(3, "");
+        pb.printDecimal(2, "");
     }
 }
