b758d270620088c01f562036a347e8ed805000ea
Jiang Huang <jiang.huang@hotmail.com>
2018-01-04 03:06:43 -0500

    Use dfs and backtarcking for print decimal problem

--- src/com/algorithms/backtracking/printBinary.java
+++ src/com/algorithms/backtracking/printBinary.java
@@ -1,6 +1,6 @@
 package com.algorithms.backtracking;
 
-public class printBinary {
+public class PrintBinary {
     public void printBinary(int digits, String  prefix) {
         if(digits == 0) {
             System.out.println(prefix);
@@ -11,18 +11,38 @@ public class printBinary {
 
     }
 
-    public void printDecimal(int digits, String prefix) {
+    public void printDecimal(int digits) {
+        printDecimalDfs(digits, "");
+        System.out.println("");
+        printDecimalBackTracking(digits, new StringBuilder());
+    }
+
+    public void printDecimalDfs(int digits, String prefix) {
         if(digits == 0) {
-            System.out.println(prefix);
+            System.out.print(prefix + " ");
         }else{
             for(int i = 0; i <= 9; i++)
-                printDecimal(digits - 1, prefix + i);
+                printDecimalDfs(digits - 1, prefix + i);
         }
     }
 
+    public void printDecimalBackTracking(int digits, StringBuilder sb) {
+        if(digits == 0) {
+            System.out.print(sb.toString() + " ");
+        }else{
+            for(int i = 0; i <= 9; i++) {
+                sb.append(i);
+                printDecimalBackTracking(digits - 1, sb);
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        printBinary pb = new printBinary();
+        PrintBinary pb = new PrintBinary();
         pb.printBinary(3, "");
-        pb.printDecimal(2, "");
+        pb.printDecimal(2);
     }
 }
