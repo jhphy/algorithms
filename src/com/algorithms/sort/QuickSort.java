393fa0ab724ecd3b694fe5819be076848864c6bb
Jiang Huang <jiang.huang@hotmail.com>
2018-01-15 23:46:42 -0500

    Mistake Repaired

--- src/com/algorithms/sort/QuickSort.java
+++ src/com/algorithms/sort/QuickSort.java
@@ -13,7 +13,7 @@ public class QuickSort {
     private int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
-        for(int j = low; j < high - 1; j++) {
+        for(int j = low; j <= high - 1; j++) {
             if(arr[j] <= pivot) {
                 i++;
                 swap(arr, i, j);
